9111641640e102c823c65d47c45fd2c7f8644fe6
ABNER JOSÉ FELIX BARBOSA <abnerbarbosa95@gmail.com>
2023-09-17 15:06:32 -0300

    committed by Abner

--- src/main/java/com/api/dashboardproject/configurations/SegurityConfiguration.java
+++ src/main/java/com/api/dashboardproject/configurations/SegurityConfiguration.java
@@ -1,28 +1,40 @@
 package com.api.dashboardproject.configurations;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SegurityConfiguration {
-
-	public void configure(HttpSecurity http) throws Exception {
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests((val) -> {
 			val.anyRequest().authenticated();
 		}).sessionManagement((val) -> val.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-				.csrf((val) -> val.disable()).build();
+				.csrf((val) -> val.disable()).httpBasic(Customizer.withDefaults());
+
+		return http.build();
 	}
 
-	public void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("user1").password("123").roles("ADMIN");
+	@Bean
+	public InMemoryUserDetailsManager userDetailsManager() {
+		UserDetails user1 = User.builder().username("user1").password(passwordEncoder().encode("123")).roles("ADMIN")
+				.build();
+
+		return new InMemoryUserDetailsManager(user1);
 	}
 
+	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
