e0eb4c9bba6522d72af104d3d09a8b1b7425ee3f
Usuário <Usurio@DESKTOP-3QRTSV1>
2023-10-12 14:40:18 -0300

    committed by Abner

--- src/main/java/com/api/projectdashboard/dtos/ResponsibleResponseDto.java
+++ src/main/java/com/api/projectdashboard/dtos/ResponsibleResponseDto.java
@@ -13,12 +13,10 @@ public class ResponsibleResponseDto {
 	private String id;
 	private String name;
 	private String email;
-	private String password;
 	
 	public ResponsibleResponseDto(ResponsibleEntity entity) {
 		this.id = entity.getId();
 		this.name = entity.getName();
 		this.email = entity.getEmail();
-		this.password =entity.getEmail();
 	}
 }
