2a20b9815e45eb603697fcd04454670e4398ba62
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2023-10-28 17:17:37 -0300

    committed by Abner

--- src/main/java/com/api/projectdashboard/dtos/ResponsibleResponseDto.java
+++ src/main/java/com/api/projectdashboard/dtos/ResponsibleResponseDto.java
@@ -13,10 +13,12 @@ public class ResponsibleResponseDto {
 	private String id;
 	private String name;
 	private String email;
+	private String password;
 	
 	public ResponsibleResponseDto(ResponsibleEntity entity) {
 		this.id = entity.getId();
 		this.name = entity.getName();
 		this.email = entity.getEmail();
+		this.password = null;
 	}
 }
